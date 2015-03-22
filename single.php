d2fd6099af7c1e356bc1905bab1a376413b5bfbb
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-03-22 13:43:05 +0900

    social buttons

--- single.php
+++ single.php
@@ -18,7 +18,23 @@
 <?php endif; ?>
 <?php the_tags('<span><i class="fa fa-tags"></i> ', ', ', '</span>'); ?>
 </div>
+
+<div class='socialbox'>
+<div class='tweetbox'>
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="hanhan1978">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 </div>
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&appId=719961888082532&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="<?php echo the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
+</div>
+<br clear='both'/>
 
 <div class="content">
 <?php the_content(); ?>
